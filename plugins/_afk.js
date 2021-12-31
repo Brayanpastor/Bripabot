9c4d8a33fd6dfa2d7f2ce78c3fb67d66257d3d95
Brayanpastor <96893713+Brayanpastor@users.noreply.github.com>
2021-12-31 15:00:42 -0500

    Update _afk.js

--- plugins/_afk.js
+++ plugins/_afk.js
@@ -3,8 +3,8 @@ handler.before = m => {
   let user = global.DATABASE.data.users[m.sender]
   if (user.afk > -1) {
     m.reply(`
-Dejaste de estar AFK${user.afkReason ? ' setelah ' + user.afkReason : ''}
-Selama ${clockString(new Date - user.afk)}
+Dejaste de estar AFK${user.afkReason ? ' despues de estarlo por ' + user.afkReason : ''}
+ Tiempo de inactividad: ${clockString(new Date - user.afk)}
 `.trim())
     user.afk = -1
     user.afkReason = ''
